beab429167f4ee28ff484496646050b70217d1b3
João Marcelo <JoaoMdSB@hotmail.com>
2024-04-14 22:45:20 -0300

    Criação de ES.h sort.h
    Bibliotecas para as funções de entrada e saída e para os algoritmos de ordenação

--- main.c
+++ main.c
@@ -2,8 +2,26 @@
     Código-Fonte para a implementação de um sistema de Benchmark
     para algoritmos de ordenação.
 
-    Desenvolvedores: Henrique Zucato e João Marcelo
     Grupo: 
 
     Criado em 14/04/2024
 */
+
+#include <stdio.h>
+#include <time.h>
+#include "ES.h"
+#include "sort.h"
+/*
+    Include das bibliotecas com funções de entrada e saída
+    e dos algoritmos de ordenação.
+*/
+
+int main() {
+    clock_t inicio, fim;
+    double t;
+    inicio = clock();
+    fim = clock();
+    t = ((float)(fim - inicio))/((CLOCKS_PER_SEC/1000));
+    printf("%.4fms\n", t);
+    return 0;
+}
