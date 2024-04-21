bbc5763fffc488b01854e3ee97e85c1df439d17e
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-04-21 16:12:56 -0300

    Update sort.c

--- sort.c
+++ sort.c
@@ -140,3 +140,147 @@ int particiona(int *v, int inicio, int fim) {
 	v[pos] = pivo;
 	return pos;
 }
+
+
+//Códigos com comparações e trocas
+
+void bubbleSort(int *vet, int tam, int *compara, int *trocas){
+    int aux;
+    for(int i = 0; i < tam; i++){
+        for(int j = 1; j < tam; j++){
+            (*compara)++;
+            if(vet[j] < vet[j-1]){
+                aux = vet[j];
+                vet[j] = vet[j-1];
+                vet[j-1] = aux;
+                (*trocas)++;
+            }
+        }
+    }
+}
+
+void bubbleSortInteligente(int *vet, int tam, int *compara, int *trocas){
+    int aux, ver = 0;
+    for(int i = 0; i < tam && ver == 0; i++){
+        for(int j = 1; j < tam - i; j++){
+            ver = 1;
+            (*compara)++;
+            if(vet[j] < vet[j-1]){
+                aux = vet[j];
+                vet[j] = vet[j-1];
+                vet[j-1] = aux;
+                ver = 0;
+                (*trocas)++;
+            }
+        }
+    }
+}
+
+int menorElemento(int *vet, int tam, int i, int *compara){
+    int menor = i;
+    for(int j = i + 1; j < tam; j++){
+        (*compara)++;
+        if(vet[j] < vet[menor]){
+            menor = j;
+        }
+    }
+    return menor;
+}
+
+void selectionSort(int *vet, int tam, int *compara, int *trocas){
+    int menor, aux;
+    for(int i = 0; i < tam - 1; i++){
+        menor = menorElemento(vet, tam, i+1, compara);
+        (*compara)++;
+        if(vet[i] > vet[menor]){
+            aux = vet[i];
+            vet[i] = vet[menor];
+            vet[menor] = aux;
+            (*trocas)++;
+        }
+    }
+}
+
+void insertionSort(int *vet, int tam, int *compara, int *trocas){
+    int aux, pos;
+    for(int i = 1; i < tam; i++){
+        pos = i - 1;
+        aux = vet[i];
+        (*compara)++;
+        while(vet[pos] > aux && pos >= 0){
+            vet[pos+1] = vet[pos];
+            pos--;
+        }
+        if(pos + 1 != i){
+            vet[pos+1] = aux;
+            (*trocas)++;
+        }
+    }
+}
+
+void merge(int *vet, int inicio, int meio, int fim, int *compara, int *trocas){
+    int v1 = inicio, v2 = meio+1, *aux = (int*)malloc(sizeof(int) * (fim - inicio + 1)), i = 0;
+    while(v1 <= meio && v2 <= fim){
+        (*compara)++;
+        if(vet[v1] < vet[v2]){
+            aux[i++] = vet[v1++];
+        }else{
+            aux[i++] = vet[v2++];
+        }
+    }
+    while(v1 <= meio){
+        aux[i++] = vet[v1++];
+    }
+    while(v2 <= fim){
+        aux[i++] = vet[v2++];
+    }
+    i = 0;
+    for(int j = inicio; j <= fim; j++){
+        (*trocas)++;
+        vet[j] = aux[i++];
+    }
+    free(aux);
+}
+
+void mergeSort(int *vet, int inicio, int fim, int *compara, int *trocas){
+    int meio = (inicio + fim)/2;
+    if(inicio < fim){
+        mergeSort(vet, inicio, meio, compara, trocas);
+        mergeSort(vet, meio+1, fim, compara, trocas);
+        merge(vet, inicio, meio, fim, compara, trocas);
+    }
+}
+
+int particiona(int *vet, int inicio, int fim, int *compara, int *trocas){
+    int pivo = vet[inicio], aux, pos = inicio;
+    for(int i = inicio + 1; i <= fim; i++){
+        (*compara)++;
+        if(vet[i] < pivo){
+            pos++;
+            (*compara)++;
+            if(pos != i){
+                aux = vet[i];
+                vet[i] = vet[pos];
+                vet[pos] = aux;
+                (*trocas)++;
+            }
+        }
+    }
+    (*compara)++;
+    if(pos != inicio){
+        aux = vet[pos];
+        vet[pos] = vet[inicio];
+        vet[inicio] = aux;
+        (*trocas)++;
+    }
+    return pos;
+}
+
+void quickSort(int *vet, int inicio, int fim, int *compara, int *trocas){
+    int pivo;
+    if(inicio < fim){
+        pivo = particiona(vet, inicio, fim, compara, trocas);
+        quickSort(vet, inicio, pivo - 1, compara, trocas);
+        quickSort(vet, pivo + 1, fim, compara, trocas);
+    }
+}
