838d8045ec771c905d017ccd7567f45fa46c26d8
Diego <144741889+ValimD@users.noreply.github.com>
2024-04-26 15:00:15 -0300

    Update ES.c

--- ES.c
+++ ES.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "ES.h"
 
-typedef struct parametros {
+struct parametros {
     long long int seed;
     int tipoOrdenacao;
     long long int tamAmostra;
-}par;
+};
 
 FILE* abreArquivo(){
     FILE* arquivo;
@@ -13,67 +14,110 @@ FILE* abreArquivo(){
     
     printf("Digite o nome do arquivo: \n");
     scanf(" %s", nomeArquivo);
-    arquivo = fopen(nomeArquivo, "w");
+    arquivo = fopen(nomeArquivo, "w+");
     
     if (!arquivo)
     {
         printf("Erro ao abrir o arquivo de entrada.\n");
-        return 1;
+        return NULL;
     }
 
     return arquivo;
 }
 
-par* obtemParametros(){
-    par* parametro;
-    parametro->tipoOrdenacao = 0;
+void obtemParametros(par* parametros){
+    parametros->tipoOrdenacao = 0;
 
-    while (parametro->tipoOrdenacao < 1 || parametro->tipoOrdenacao > 4)
+    while (parametros->tipoOrdenacao < 1 || parametros->tipoOrdenacao > 4)
     {
         printf("Qual tipo de conjunto de números você deseja que seja criado (1 - Conjunto aleatório, 2 - Conjunto Crescente ,3 - Conjunto Semi-Ordenado, 4 - Conjunto Decrescente)?\n");
-        scanf(" %d", &parametro->tipoOrdenacao);
+        scanf(" %d", &parametros->tipoOrdenacao);
     }
 
     printf("Qual o tamanho da amostra a ser criada?\n");
-    scanf(" %lld", parametro->tamAmostra);
+    scanf(" %lld", &parametros->tamAmostra);
 
-    if (parametro->tipoOrdenacao == 1 || parametro->tipoOrdenacao == 3)
+    if (parametros->tipoOrdenacao == 1 || parametros->tipoOrdenacao == 3)
     {
-        printf("Qual a semente da geração de numeros pseudo-aleatorios sera utilizada?\n")/
-        scanf(" %lld", &parametro->seed);
-        srand(parametro->seed);
+        printf("Qual a semente da geração de numeros pseudo-aleatorios sera utilizada?\n");
+        scanf(" %lld", &parametros->seed);
+        srand(parametros->seed);
     }
-    return parametro;
+    return;
 }
 
-void geraAmostraAleatoria(par parametro, FILE* arquivo){
-    srand(parametro.seed);
-    for(int i = 0 ; i < parametro.tamAmostra ; i++){
-                fprintf(arquivo, " %d\n", rand());
+void geraAmostraAleatoria(par *parametro, FILE* arquivo){
+    rewind(arquivo);
+    fprintf(arquivo, "%lld\n", parametro->tamAmostra);
+    fprintf(arquivo, "%lld\n", parametro->seed);
+    srand(parametro->seed);
+    for(int i = 0 ; i < parametro->tamAmostra ; i++){
+                fprintf(arquivo, "%d\n", rand());
             }
 }
 
-void geraAmostraCrescente(par parametro, FILE* arquivo){
-    srand(parametro.seed);
-    for(int i=0 ; i<parametro.tamAmostra ; i++){
-                fprintf(arquivo, " %d\n", i);
+void geraAmostraCrescente(par* parametro, FILE* arquivo){
+    rewind(arquivo);
+    fprintf(arquivo, "%lld\n", parametro->tamAmostra);
+    fprintf(arquivo, "%lld\n", parametro->seed);
+    srand(parametro->seed);
+    for(int i=0 ; i<parametro->tamAmostra ; i++){
+                fprintf(arquivo, "%d\n", i);
             }
 }
 
-void geraAmostraSemiOrdenada(par parametro, FILE* arquivo){
-    srand(parametro.seed);
-    for(int i=0 ; i < parametro.tamAmostra ; i++){
-                if (i < parametro.tamAmostra/10){
-                    fprintf(arquivo, " %d\n", rand());
+void geraAmostraSemiOrdenada(par* parametro, FILE* arquivo){
+    rewind(arquivo);
+    fprintf(arquivo, "%lld\n", parametro->tamAmostra);
+    fprintf(arquivo, "%lld\n", parametro->seed);
+    srand(parametro->seed);
+    for(int i=0 ; i < parametro->tamAmostra ; i++){
+                if (i < parametro->tamAmostra/10){
+                    fprintf(arquivo, "%d\n", rand());
                 }else{
-                    fprintf(arquivo, " %d\n", i);
+                    fprintf(arquivo, "%d\n", i);
                 }
             }
 }
 
-void geraAmostraDescrescente(par parametro, FILE* arquivo){
-    srand(parametro.seed);
-    for(int i = parametro.tamAmostra ; i > 0 ; i--){
-                fprintf(arquivo, " %d\n", i);
+void geraAmostraDescrescente(par* parametro, FILE* arquivo){
+    rewind(arquivo);
+    fprintf(arquivo, "%lld\n", parametro->tamAmostra);
+    fprintf(arquivo, "%lld\n", parametro->seed);
+    srand(parametro->seed);
+    for(int i = parametro->tamAmostra ; i > 0 ; i--){
+                fprintf(arquivo, "%d\n", i);
             }
+}
+
+int* criaVetor(FILE* arquivo){
+    int* vetor;
+    long long int tamVetor;
+    long long int seed;
+    rewind(arquivo);
+    fscanf(arquivo, " %lld", &tamVetor);
+    fscanf(arquivo, " %lld", &seed);
+    vetor = (int*)malloc(sizeof(int)*tamVetor);
+    if (!vetor)
+    {
+        printf("Ocorreu um erro durante a alocação do vetor!\n");
+        return NULL;
+    }
+    
+    for(int i=0 ; i < tamVetor ; i++){
+        fscanf(arquivo, " %d", &vetor[i]);
+    }
+    return vetor;
+}
+
+void imprimeVetor(int* vetor, FILE* arquivo){
+    long long int tamVetor;
+    rewind(arquivo);
+    fscanf(arquivo, " %lld", &tamVetor);
+    printf("Vetor = [");
+    for (int i = 0 ; i < tamVetor-1 ; i++)
+    {
+        printf("%d, ", vetor[i]);
+    }
+    printf("%d]\n", vetor[tamVetor-1]);
 }
