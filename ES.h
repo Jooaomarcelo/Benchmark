69d81fac28d27372c901c21116e67f21853caf64
Jooaomarcelo <joaomdsb@hotmail.com>
2024-04-27 18:50:58 -0300

    Update ES.h

--- ES.h
+++ ES.h
@@ -16,35 +16,4 @@ int* geraQuaseOrdenados(long long int tam, int porcentagem, int semente);
 
 int* geraOrdenados(long long int tam, int ordem);
 
-void escolhaOrdenacao(vetor *v);
-
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct par {
-    long long int seed;
-    int tipoOrdenacao;
-    long long int tamAmostra;
-}par;
-
-typedef struct vet {
-    int *v;
-    long long int tamanho;
-    long long int seed;
-}vet;
-
-FILE* abreArquivo();
-
-par* obtemParametros(par* parametros);
-
-void geraAmostraAleatoria(par* parametro, FILE* arquivo);
-
-void geraAmostraCrescente(par* parametro, FILE* arquivo);
-
-void geraAmostraSemiOrdenada(par* parametro, FILE* arquivo);
-
-void geraAmostraDescrescente(par* parametro, FILE* arquivo);
-
-vet* criaVetor(FILE* arquivo, vet *vetor);
-
-void imprimeVetor(int* vetor, long long int tamanho);
+void escolhaOrdenacao(vetor *v, int semente, int tipoOrdenacao, int porcentagem);
