ac03e4a832e125ae1488b39d8a9ece7facd22f07
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-04-27 19:19:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,131 +1,77 @@
-/*
-    Código-Fonte para a implementação de um sistema de Benchmark
-    para algoritmos de ordenação.
-
-    Grupo: 
-
-    Criado em 14/04/2024
-*/
-
 #include <stdio.h>
-#include <time.h>
-#include "ES.h"
 #include "sort.h"
-/*
-    Include das bibliotecas com funções de entrada e saída
-    e dos algoritmos de ordenação.
-*/
+#include "ES.h"
+#include <stdlib.h>
 
-int main() {
-    clock_t inicio, fim;
-    FILE* arquivo;
-    par *params;
-    vet *vetor;
-    char nomeArquivo[31];
-    double t;
-    int controle_criacao=0, controle_ordenacao=0, tipoOrdenacao=0;
-    int trocas=0, compara=0;
-
-
-    printf("----- Sistema de Benchmark -----\n");
-    printf("O usuário ja possui os arquivos a serem analisados?\n0. Não\n1. Sim\n");
-    scanf(" %d", &controle_criacao);
-    if(controle_criacao == 0) {
-        do {
-            arquivo = abreArquivo(arquivo);
-            params = obtemParametros(params);
-            switch (params->tipoOrdenacao){
-            case 1:
-                geraAmostraAleatoria(params, arquivo);
-                break;
-            
-            case 2:
-                geraAmostraCrescente(params, arquivo);
-                break;
-            
-            case 3:
-                geraAmostraSemiOrdenada(params, arquivo);
-                break;
-            
-            case 4:
-                geraAmostraDescrescente(params, arquivo);
-                break;
-            
-            default:
-                break;
-            }
-
-            fclose(arquivo);
-            printf("Todos os arquivos foram criados?\n0. Não\n1. Sim\n");
-            scanf(" %d", &controle_criacao);
-        } while(controle_criacao == 0);
+void printa(long long int *vet, long long int tam){
+    printf("\nPrintando vetor: ");
+    for(int i = 0; i < tam; i++){
+        printf("%lld ", vet[i]);
     }
+}
+
+int main(void) {
+    vetor *v = criaVetor();
+    int qualOrdenacao = 7, tipoOrdenacao = 4, semente = 15, porcentagem = 10;
+    long long *vet;
+    FILE *file = fopen("C:\\Users\\Usuario\\Documents\\01 - Universidade\\3 - Periodo\\CTCO02 - Algoritimo e Estruturas de Dados II\\8_Trabalho1\\Teste.txt", "w");
     
-    while(controle_ordenacao == 0){
-        arquivo = abreArquivo(arquivo);
-        vetor = criaVetor(arquivo, vetor);
-        imprimeVetor(vetor->v, vetor->tamanho);
-        printf("Tipo de ordenação que se deseja analisar:\n1. Bolha\n2. Bolha Inteligente\n3. Seleção\n4. Inserção\n5. Merge-Sort\n6. Quick-Sort\n7. Dual-Pivot Quick-Sort");
-        scanf(" %d", &tipoOrdenacao);
-        trocas=0, compara=0;
-        switch (tipoOrdenacao){
-        case 1:
-            inicio = clock();
+    printf("\n1 - BS, 2 - BSI, 3 - S, 4 - I, 5 - M, 6 - Q, 7 - QD");
+    printf("\nDigite o qual ordenação você usará:");
+    scanf("%d", &qualOrdenacao);
 
-            bubbleSort(vetor->v, vetor->tamanho, &compara, &trocas);
+    while(qualOrdenacao > 0 && qualOrdenacao < 8){
+        printf("\n\n1 - Aleatório, 2 - Semi-Aleatorio, 3 - Crescente, 4 - Decrescente");
+        printf("\nDigite o tipo de ordenação: ");
+        scanf("%d", &tipoOrdenacao);
 
-            fim = clock();
+        escolhaOrdenacao(v, semente, tipoOrdenacao, porcentagem);
 
-            t = ((float)(fim - inicio))/((CLOCKS_PER_SEC/1000));
-            printf("Tempo gasto: %.4fms\nNº de trocas: %d, Nº de comparações: %d\n", t, trocas, compara);
-            break;
+        switch(qualOrdenacao){
+            case 1:
+                printf("\nBubbleSort");
+                bubbleSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
+                break;
+            case 2:
+                printf("\nBubbleSortInteligênte");
+                bubbleSortInteligente(getVet(v), getTam(v), getCompara(v), getTrocas(v));
+                break;
+            case 3:
+                printf("\nSelectionSort");
+                selectionSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
+                break;
+            case 4:
+                printf("\nInsertionSort");
+                insertionSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
+                break;
+            case 5:
+                printf("\nMergeSort");
+                mergeSort(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
+                break;
+            case 6:
+                printf("\nQuickSort");
+                quickSort(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
+                break;
+            default:
+                printf("\nQuickSortDualPivot");
+                quickSortDual(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
+        }
         
-        case 2:
-            inicio = clock();
+        printf("\nComparações: %ld\nTrocas: %ld", *getCompara(v), *getTrocas(v));
 
-            bubbleSortInteligente(vetor->v, vetor->tamanho, &compara, &trocas);
-
-            fim = clock();
-            t = ((float)(fim - inicio))/((CLOCKS_PER_SEC/1000));
-            printf("Tempo gasto: %.4fms\nNº de trocas: %d, Nº de comparações: %d\n", t, trocas, compara);
-            break;
-
-        case 3:
-            inicio = clock();
-
-            bubbleSortInteligente(vetor->v, vetor->tamanho, &compara, &trocas);
-
-            fim = clock();
-            t = ((float)(fim - inicio))/((CLOCKS_PER_SEC/1000));
-            printf("Tempo gasto: %.4fms\nNº de trocas: %d, Nº de comparações: %d\n", t, trocas, compara);
-            break;
-        
-        case 4:
-            inicio = clock();
-
-            bubbleSortInteligente(vetor->v, vetor->tamanho, &compara, &trocas);
-
-            fim = clock();
-            t = ((float)(fim - inicio))/((CLOCKS_PER_SEC/1000));
-            printf("Tempo gasto: %.4fms\nNº de trocas: %d, Nº de comparações: %d\n", t, trocas, compara);
-            break;
-        
-        case 5:
-            inicio = clock();
-
-            bubbleSortInteligente(vetor->v, vetor->tamanho, &compara, &trocas);
-
-            fim = clock();
-            t = ((float)(fim - inicio))/((CLOCKS_PER_SEC/1000));
-            printf("Tempo gasto: %.4fms\nNº de trocas: %d, Nº de comparações: %d\n", t, trocas, compara);
-            break;
-        
-        default:
-            break;
+        vet = getVet(v);
+        for(long long int i = 0; i < getTam(v); i++){
+            fprintf(file, "%lld\n", vet[i]);
         }
 
-        printf("Deseja continuar analisando os tipos de ordenação?\n0. Sim\n1. Não\n");
-        scanf(" %d", &controle_ordenacao);
-    } 
+        free(getVet(v));
+        setCompara(v);
+        setTrocas(v);
+
+        
+        printf("\n\n1 - BS, 2 - BSI, 3 - S, 4 - I, 5 - M, 6 - Q, 7 - QD");
+        printf("\nDigite o qual ordenação você usará:");
+        scanf("%d", &qualOrdenacao);
+    }
     return 0;
 }
