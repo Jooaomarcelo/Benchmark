3293c2110a4f574368a5d8d34f778d05e82e5fd2
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-04-27 21:37:31 -0300

    Update ES.h

--- ES.h
+++ ES.h
@@ -3,7 +3,15 @@
 
 typedef struct vetor vetor;
 
+typedef struct metricas metricas;
+
+metricas *criaMetricas();
+
+void setTabela(metricas *tabela, int tipo, int entrada, int metodo, double tempo, double comparacoes, double troca);
+
 vetor *criaVetor();
+
+void setTam(vetor *v, long long int tam);
 
 long long int *getVet(vetor *v);
 
@@ -19,10 +27,12 @@ long long int getTam(vetor *v);
 
 long long int* geraAleatorios(long long int tam, int semente);
 
-long long int* geraQuaseOrdenados(long long int tam, int porcentagem);
+long long int* geraQuaseOrdenados(long long int tam, int porcentagem, int semente);
 
 long long int* geraOrdenados(long long int tam, int ordem);
 
 void escolhaOrdenacao(vetor *v, int semente, int tipoOrdenacao, int porcentagem);
+
+void printMatriz(metricas *tabela, int metodo);
 
 #endif //INC_8_TRABALHO1_ES_H
