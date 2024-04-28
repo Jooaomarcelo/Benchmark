5e7386aa60cca8b14774bc78856f22e9f276d70f
Henrique Zucato <144706457+HenriUz@users.noreply.github.com>
2024-04-28 11:03:06 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,144 +1,126 @@
-#include <stdio.h>
+
+/* INCLUDES */
+
 #include "sort.h"
 #include "ES.h"
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
+/* FUNÇÕES */
+
+//Função responsável por chamar o respectivo método de ordenação.
+double chamaOrdenacao(vetor *v, int metodos){
+    clock_t inicio, fim; //Variáveis para calcularem o tempo
+    switch (metodos) {
+        case 0:
+            inicio = clock();
+            selectionSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
+            fim = clock();
+            break;
+        case 1:
+            inicio = clock();
+            insertionSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
+            fim = clock();
+            break;
+        case 2:
+            inicio = clock();
+            mergeSort(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
+            fim = clock();
+            break;
+        case 3:
+            inicio = clock();
+            quickSort(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
+            fim = clock();
+            break;
+        default:
+            inicio = clock();
+            quickSortDual(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
+            fim = clock();
+    }
+    //Retornando o tempo de execução de respectivo método em mili segundos
+    return (double)(fim - inicio)/(CLOCKS_PER_SEC/1000);
+}
+
+//Função principal
 int main(void) {
-    long long int entradas[5] = {1000, 10000, 100000, 1000000, 10000000};
-    int semente; //Escolhendo semente
-    double t, media, comparacoes, troca;
+    long long int entradas;
+    int semente, linha;
+    long double t, media, comparacoes, troca;
+
     vetor *v = criaVetor();
     metricas *tabela = criaMetricas();
-    clock_t inicio, fim;
 
-    //Entradas 1000 - 10000 - 100000 - 1000000 - 10000000
-    //Tipos: Aleatório - Crescente - Decrescente - Semi-Ordenado
+    //Entradas 1000 - 10000 - 100000 - 500000 - 1000000
+    entradas = 1000000;
+    //Linha da entrada na matriz da tabela
+    linha = 3;
 
-    //Percorrendo os 7 tipos de métodos de ordenação
-    for(int metodos = 0; metodos < 1; metodos++){
+    //Tipos: Aleatório - Semi-Ordenado - Crescente - Decrescente
+
+    //Percorrendo os 5 tipos de métodos de ordenação
+    for(int metodos = 2; metodos < 5; metodos++){
+
+        printf("\n%d\n", metodos);
+
         //Percorrendo os tipos
         for(int tipos = 0; tipos < 4; tipos++){
-            //Percorrendo as entradas
-            for(int entrada = 0; entrada < 5; entrada++){
-                setTam(v, entradas[entrada]);
-                media = troca = comparacoes = 0;
+            printf("Tipo: %d ", tipos);
 
-                //Verificando se os dados estarão aleatórios/desordenados
-                if(tipos == 0 || tipos == 3){
-                    //Para garantir a média
-                    for(int i = 0; i < 5; i++){
-                        semente = i;
+            //Setando o tamanho do vetor para o tamanho da entrada
+            setTam(v, entradas);
 
-                        //Cria vetor
-                        escolhaOrdenacao(v, semente, tipos, 10);
+            //Setando variáveis auxiliares para a média dos valores
+            media = troca = comparacoes = 0;
 
-                        //Chama método de ordenação
-                        switch (metodos) {
-                            case 0:
-                                inicio = clock();
-                                bubbleSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
-                                fim = clock();
-                                break;
-                            case 1:
-                                inicio = clock();
-                                bubbleSortInteligente(getVet(v), getTam(v), getCompara(v), getTrocas(v));
-                                fim = clock();
-                                break;
-                            case 2:
-                                inicio = clock();
-                                selectionSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
-                                fim = clock();
-                                break;
-                            case 3:
-                                inicio = clock();
-                                insertionSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
-                                fim = clock();
-                                break;
-                            case 4:
-                                inicio = clock();
-                                mergeSort(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
-                                fim = clock();
-                                break;
-                            case 5:
-                                inicio = clock();
-                                quickSort(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
-                                fim = clock();
-                                break;
-                            default:
-                                inicio = clock();
-                                quickSortDual(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
-                                fim = clock();
-                        }
+            //Verificando se os dados estarão aleatórios/desordenados
+            if(tipos == 0 || tipos == 1){
 
-                        //Salva tempo, salva comparação, salva trocas
-                        t = ((float)(fim - inicio))/((CLOCKS_PER_SEC/1000));
-                        media += t;
-                        comparacoes += *getCompara(v);
-                        troca += *getTrocas(v);
+                //Calculando a média de 5 execuções
+                for(int i = 0; i < 5; i++){
 
-                        setTrocas(v);
-                        setCompara(v);
+                    //Setando a semente
+                    semente = i;
 
-                        free(getVet(v));
-                    }
-                    //Salva a média na matriz
-                    setTabela(tabela, tipos, entrada, metodos, t/5, comparacoes/5, troca/5);
-                }else{
                     //Cria vetor
                     escolhaOrdenacao(v, semente, tipos, 10);
 
                     //Chama método de ordenação
-                    switch (metodos) {
-                        case 0:
-                            inicio = clock();
-                            bubbleSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
-                            fim = clock();
-                            break;
-                        case 1:
-                            inicio = clock();
-                            bubbleSortInteligente(getVet(v), getTam(v), getCompara(v), getTrocas(v));
-                            fim = clock();
-                            break;
-                        case 2:
-                            inicio = clock();
-                            selectionSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
-                            fim = clock();
-                            break;
-                        case 3:
-                            inicio = clock();
-                            insertionSort(getVet(v), getTam(v), getCompara(v), getTrocas(v));
-                            fim = clock();
-                            break;
-                        case 4:
-                            inicio = clock();
-                            mergeSort(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
-                            fim = clock();
-                            break;
-                        case 5:
-                            inicio = clock();
-                            quickSort(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
-                            fim = clock();
-                            break;
-                        default:
-                            inicio = clock();
-                            quickSortDual(getVet(v), 0, getTam(v) - 1, getCompara(v), getTrocas(v));
-                            fim = clock();
-                    }
+                    t = chamaOrdenacao(v, metodos);
 
-                    //Salva tempo, salva comparação, salva trocas
-                    t = ((float)(fim - inicio))/((CLOCKS_PER_SEC/1000));
-                    setTabela(tabela, tipos, entrada, metodos, t, (double)*getCompara(v), (double)*getTrocas(v));
-                    setCompara(v);
+                    //Preparando para calcular a média
+                    media += t;
+                    comparacoes += *getCompara(v);
+                    troca += *getTrocas(v);
+
+                    //Setando variáveis de comparações e trocas para 0
                     setTrocas(v);
+                    setCompara(v);
 
                     free(getVet(v));
                 }
+                //Salva a média na matriz
+                setTabela(tabela, tipos, linha, metodos, t/5.0, comparacoes/5.0, troca/5.0);
+            }else{
+                //Cria vetor
+                escolhaOrdenacao(v, semente, tipos, 10);
+
+                //Chama método de ordenação
+                t = chamaOrdenacao(v, metodos);
+
+                //Salva os dados na tabela
+                setTabela(tabela, tipos, linha, metodos, t, (double)*getCompara(v), (double)*getTrocas(v));
+
+                //Setando variáveis de comparações e trocas para 0
+                setCompara(v);
+                setTrocas(v);
+
+                free(getVet(v));
             }
         }
     }
 
-    for(int metodos = 0; metodos < 1; metodos++){
+    for(int metodos = 2; metodos < 5; metodos++){
         printMatriz(tabela, metodos);
     }
 
