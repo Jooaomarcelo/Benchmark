2d5b7602490392fde398de1b7294a10750368603
João Marcelo <JoaoMdSB@hotmail.com>
2024-04-29 09:54:59 -0300

    Update ES.c

--- ES.c
+++ ES.c
@@ -1,29 +1,52 @@
+#define pos 4
+
 #include "ES.h"
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 struct vetor{
     long long int *vet;
-    long int compara, trocas;
+    long long int compara, trocas;
     long long int tam;
 };
 
-//Setando variáveis importantes para o sistema
+struct metricas{
+    long double tempo[pos];
+    long double compara[pos];
+    long double trocas[pos];
+};
+
+metricas *criaMetricas(){
+    metricas *tabela = (metricas*)malloc(sizeof(metricas));
+    return tabela;
+}
+
+void setTabela(metricas *tabela, int tipo, long double tempo, long double comparacoes, long double troca){
+    tabela->tempo[tipo] = tempo;
+    tabela->compara[tipo] = comparacoes;
+    tabela->trocas[tipo] = troca;
+}
+
 vetor *criaVetor(){
     vetor *v = (vetor*)malloc(sizeof(vetor));
-
-    printf("\nQual o tamanho do vetor?");
-    scanf("%lld", &v->tam);
     v->compara = v->trocas = 0;
-
     return v;
+}
+
+long long int getTam(vetor *v){
+    return v->tam;
+}
+
+void setTam(vetor *v, long long int tam){
+    v->tam = tam;
 }
 
 long long int *getVet(vetor *v){
     return v->vet;
 }
 
-long int *getCompara(vetor *v){
+long long int *getCompara(vetor *v){
     return &v->compara;
 }
 
@@ -31,16 +54,12 @@ void setCompara(vetor *v){
     v->compara = 0;
 }
 
-long int *getTrocas(vetor *v){
+long long int *getTrocas(vetor *v){
     return &v->trocas;
 }
 
 void setTrocas(vetor *v){
     v->trocas = 0;
-}
-
-long long int getTam(vetor *v){
-    return v->tam;
 }
 
 long long int* geraAleatorios(long long int tam, int semente){
@@ -52,11 +71,13 @@ long long int* geraAleatorios(long long int tam, int semente){
     return vet;
 }
 
-long long int* geraQuaseOrdenados(long long int tam, int porcentagem){
+long long int* geraQuaseOrdenados(long long int tam, int porcentagem, int semente){
     long long int *vet = (long long int*)malloc(sizeof(long long int) * tam), valorPorcentagem = (long long int)((long double)tam * ((float)porcentagem/100.0));
+    //srand(time(NULL));
+    srand(semente);
     for(long long int i = 0; i < tam; i++){
         if(i < valorPorcentagem){
-            vet[i] = (tam*2) - i;
+            vet[i] = rand() + tam;
         }else{
             vet[i] = i;
         }
@@ -79,13 +100,29 @@ long long int* geraOrdenados(long long int tam, int ordem){
 }
 
 void escolhaOrdenacao(vetor *v, int semente, int tipoOrdenacao, int porcentagem){
-    if(tipoOrdenacao == 1){
+    if(tipoOrdenacao == 0){
         v->vet = geraAleatorios(v->tam, semente);
+    }else if(tipoOrdenacao == 1){
+        v->vet = geraQuaseOrdenados(v->tam, porcentagem, semente);
     }else if(tipoOrdenacao == 2){
-        v->vet = geraQuaseOrdenados(v->tam, porcentagem);
-    }else if(tipoOrdenacao == 3){
         v->vet = geraOrdenados(v->tam, 0);
     }else{
         v->vet = geraOrdenados(v->tam, 1);
+    }
+}
+
+void printVetor(metricas *tabela){
+    for(int t = 0; t < 3; t++) {
+        printf("\n\n");
+        for (int j = 0; j < pos; j++) {
+            if(t == 0){
+                printf("%Lf ", tabela->tempo[j]);
+            }else if(t == 1){
+                printf("%Lf ", tabela->compara[j]);
+            }else{
+                printf("%Lf ", tabela->trocas[j]);
+            }
+        }
+        printf("\n");
     }
 }
